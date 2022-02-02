8fbd1d92dbf70d366411e4269c79c379c50d9321
sathiyan.A <saththiyan97@gmail.com>
2022-02-02 19:59:20 +0530

    Connected to postgres server

--- src/main/java/main/java/Customer.java
+++ src/main/java/main/java/Customer.java
@@ -1,3 +1,5 @@
+package main.java;
+
 public class Customer {
     private Account accountNo;
     private String name;
--- src/main/java/main/java/httpMirror.java
+++ src/main/java/main/java/httpMirror.java
@@ -1,7 +1,10 @@
+package main.java;
+
 import java.io.*;
 import java.math.BigDecimal;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,6 +22,7 @@ class HttpMirror {
     Bank banks = new Bank();
 
     public static void main(String[] args) {
+
         HttpMirror obj = new HttpMirror();
         Account acc = new Account();
         try {
@@ -73,16 +77,16 @@ class HttpMirror {
                     out.println("");
 //
                     StringBuilder webHtml = new StringBuilder();
-                    FileReader fileReader = new FileReader("/Users/sathiyan/Documents/Bank app using HTTP/src/web/web.html");
+                    FileReader fileReader = new FileReader("/Users/sathiyan/Documents/Bank app using HTTP/src/main/java/main/java/web/web.html");
                     BufferedReader bufferReader = new BufferedReader(fileReader);
                     String val;
                     while ((val = bufferReader.readLine()) != null) {
                         webHtml.append(val);
                     }
                     String result = webHtml.toString();
-
+//                    out.print(webHtml);
                     out.write(result);
-                    out.write("\r\n\r\n");
+//                    out.write("\r\n\r\n");
 
                 } else if (method.equals("POST")) {
 //                    System.out.println("Hello POST");
@@ -104,31 +108,36 @@ class HttpMirror {
                     String[] data3 = contentData[2].split("=");
                     String alloutput;
 //                    System.out.println("URI = " + URI.trim());
+
+
                     switch (URI) {
                         case "/create":
-                            alloutput = obj.accountCreate(data1[1], data2[1], data3[1]);
-
-                            out.write(alloutput);
+                            obj.connect(data1[1], data2[1], data3[1]);
+//                            alloutput = obj.accountCreate(data1[1], data2[1], data3[1]);
+//                            out.write(alloutput);
                             break;
                         case "/deposit":
                             int accNum = Integer.parseInt(data2[1]);
                             double amount = Double.parseDouble(data3[1]);
-                            alloutput = obj.makeDeposit(accNum, amount);
-                            out.write(alloutput);
+                            obj.AmountDepo(accNum, amount);
+//                            alloutput = obj.makeDeposit(accNum, amount);
+//                            out.write(alloutput);
                             break;
 
                         case "/withdrawal":
                             int accNum1 = Integer.parseInt(data2[1]);
                             double amount1 = Double.parseDouble(data3[1]);
-                            alloutput = obj.makeWithdrawal(accNum1, amount1);
-                            out.write(alloutput);
+                            obj.AmountWith(accNum1, amount1);
+//                            alloutput = obj.makeWithdrawal(accNum1, amount1);
+//                            out.write(alloutput);
                             break;
                         case "/transfer":
                             int fromAccNum = Integer.parseInt(data1[1]);
                             int toAccNum = Integer.parseInt(data2[1]);
                             double amount2 = Double.parseDouble(data3[1]);
-                            alloutput = obj.makeTransfer(fromAccNum, toAccNum, amount2);
-                            out.write(alloutput);
+                            obj.AmountTransfer(fromAccNum,toAccNum,amount2);
+//                            alloutput = obj.makeTransfer(fromAccNum, toAccNum, amount2);
+//                            out.write(alloutput);
                             break;
 
 
@@ -150,6 +159,223 @@ class HttpMirror {
         }
     }
 
+    String name;
+    String nic;
+    String address;
+
+    public void connect(String name, String nic, String address) {
+        this.name = name;
+        this.nic = nic;
+        this.address = address;
+
+        try {
+
+            String user = "postgres";
+            String password = "0000";
+            String url = "jdbc:postgresql://localhost:8001/sampleDB";
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+
+
+            Statement stmt = conn.createStatement();
+
+            // the mysql insert statement
+            String query = " insert into \"bank\".customers (\"Name\", \"NIC\", \"Address\")"
+                    + " values (?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString(1, name);
+            preparedStmt.setString(2, nic);
+            preparedStmt.setString(3, address);
+
+
+            boolean x = preparedStmt.execute();
+
+            if (!x) {
+                System.out.println("Successfully Inserted");
+            } else {
+                System.out.println("Insert Failed");
+            }
+
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    int accNum;
+    double amt;
+
+    public void AmountDepo(int acc, double amt1) {
+        double newAmt = 0;
+        this.accNum = acc;
+        this.amt = amt1;
+        try {
+
+            String user = "postgres";
+            String password = "0000";
+            String url = "jdbc:postgresql://localhost:8001/sampleDB";
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            String getAmt = "select \"Amount\"\n" +
+                    "from \"bank\".customers\n" +
+                    "where  \"AccountNo\"= ?;";
+            PreparedStatement preparedStmt1 = conn.prepareStatement(getAmt);
+            preparedStmt1.setInt(1, accNum);
+            ResultSet rs = preparedStmt1.executeQuery();
+
+
+            if (rs.next()) {
+
+                newAmt = rs.getDouble(1);
+                System.out.println(newAmt);
+            }
+
+
+            // create the java mysql update preparedstatement
+            String query = "UPDATE bank.customers\n" +
+                    "\tSET  \"Amount\"=?\n" +
+                    "\tWHERE \"AccountNo\" = ?;";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setDouble(1, (amt + newAmt));
+            preparedStmt.setInt(2, accNum);
+
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    int accNum1;
+    double amt1;
+
+    public void AmountWith(int acc, double amt1) {
+        double newAmt = 0;
+        this.accNum1 = acc;
+        this.amt1 = amt1;
+        try {
+
+            String user = "postgres";
+            String password = "0000";
+            String url = "jdbc:postgresql://localhost:8001/sampleDB";
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            String getAmt = "select \"Amount\"\n" +
+                    "from \"bank\".customers\n" +
+                    "where  \"AccountNo\"= ?;";
+            PreparedStatement preparedStmt1 = conn.prepareStatement(getAmt);
+            preparedStmt1.setInt(1, accNum1);
+            ResultSet rs = preparedStmt1.executeQuery();
+
+
+            if (rs.next()) {
+
+                newAmt = rs.getDouble(1);
+//                System.out.println(newAmt);
+            }
+
+
+            // create the java mysql update preparedstatement
+            String query = "UPDATE bank.customers\n" +
+                    "\tSET  \"Amount\"=?\n" +
+                    "\tWHERE \"AccountNo\" = ?;";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setDouble(1, (newAmt - amt1));
+            preparedStmt.setInt(2, accNum1);
+
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    int fromaccNum;
+    int toaccNum;
+    double amt2;
+
+    public void AmountTransfer(int acc, int acc2, double amt1) {
+        double newAmtto = 0;
+        double newAmtfrom = 0;
+        this.toaccNum = acc2;
+        this.fromaccNum = acc;
+        this.amt2 = amt1;
+
+        try {
+
+            String user = "postgres";
+            String password = "0000";
+            String url = "jdbc:postgresql://localhost:8001/sampleDB";
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            String getAmt = "select \"Amount\"\n" +
+                    "from \"bank\".customers\n" +
+                    "where  \"AccountNo\"= ?;";
+            PreparedStatement preparedStmt1 = conn.prepareStatement(getAmt);
+            preparedStmt1.setInt(1, fromaccNum);
+            ResultSet rs = preparedStmt1.executeQuery();
+
+
+            if (rs.next()) {
+
+                newAmtfrom = rs.getDouble(1);
+//                System.out.println(newAmt);
+            }
+
+
+            // create the java mysql update preparedstatement
+            String query = "UPDATE bank.customers\n" +
+                    "\tSET  \"Amount\"=?\n" +
+                    "\tWHERE \"AccountNo\" = ?;";
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setDouble(1, (newAmtfrom - amt2));
+            preparedStmt.setInt(2, fromaccNum);
+
+            // execute the java preparedstatement
+            preparedStmt.executeUpdate();
+
+
+
+            String getAmt1 = "select \"Amount\"\n" +
+                    "from \"bank\".customers\n" +
+                    "where  \"AccountNo\"= ?;";
+            PreparedStatement preparedStmt2 = conn.prepareStatement(getAmt1);
+            preparedStmt2.setInt(1, toaccNum);
+            ResultSet rs1 = preparedStmt2.executeQuery();
+
+
+            if (rs1.next()) {
+
+                newAmtto = rs1.getDouble(1);
+//                System.out.println(newAmt);
+            }
+
+
+            // create the java mysql update preparedstatement
+            String query1 = "UPDATE bank.customers\n" +
+                    "\tSET  \"Amount\"=?\n" +
+                    "\tWHERE \"AccountNo\" = ?;";
+            PreparedStatement preparedStmt3 = conn.prepareStatement(query1);
+            preparedStmt3.setDouble(1, (newAmtto + amt2));
+            preparedStmt3.setInt(2, toaccNum);
+
+            // execute the java preparedstatement
+            preparedStmt3.executeUpdate();
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public String accountCreate(String name, String nic, String address) {
         Account accountNo = new Account();
@@ -160,6 +386,7 @@ class HttpMirror {
     }
 
     public String makeDeposit(int accountNo, double amount) {
+
         if (banks.getCustomerIndex(accountNo) < 0) {
             return "Invalid account number";
         } else {
